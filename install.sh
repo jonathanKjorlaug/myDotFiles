28445ab875cfe98bce35a4df5f4b3ad3c74427a1
Jonathan Kjorlaug <jonathan@kjorlaug.no>
2024-04-01 16:50:57 +0200

    Updated to add tmux and partial nvim configuration

--- install.sh
+++ install.sh
@@ -1,13 +1,1 @@
-#!/bin/zsh
-CURRENT_PATH=~/.config/personalConfig
-
-cd ~/.config/
-
-ln -s $CURRENT_PATH/neofetch neofetch
-ln -s $CURRENT_PATH/nvim nvim
-ln -s $CURRENT_PATH/espanso espanso
-ln -s $CURRENT_PATH/rclone rclone
-
-cd ~
-ln -s $CURRENT_PATH/.zsh .zsh
-ln -s $CURRENT_PATH/.zsh/.zshrc .zshrc
+/home/jonathankjorlaug/.config/personalConfig/install.sh
