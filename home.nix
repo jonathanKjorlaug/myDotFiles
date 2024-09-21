d85bc985268878e2b92cf76af44a8ccfd99ff1eb
Jonathan Kjørlaug <jonathan@kjorlaug.no>
2024-09-21 23:24:46 +0200

    Should be a working equivalent now...

--- home.nix
+++ home.nix
@@ -1,46 +1,49 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "jonathankjorlaug";
-  home.homeDirectory = "/home/jonathankjorlaug";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = "jonathankjorlaug";
+    home.homeDirectory = "/home/jonathankjorlaug";
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+    home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  home.packages = [
-    (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; })
+    home.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; })
+        pkgs.kitty
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+        # # You can also create simple shell scripts directly inside your
+        # # configuration. For example, this adds a command 'my-hello' to your
+        # # environment:
+        # (pkgs.writeShellScriptBin "my-hello" ''
+        #   echo "Hello, ${config.home.username}!"
+        # '')
+    ];
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
+    home.file = {
+        # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+        # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+        # # symlink to the Nix store copy.
+        # ".screenrc".source = dotfiles/screenrc;
 
-    xdg.configfile = {
+        # # You can also set the file content immediately.
+        # ".gradle/gradle.properties".text = ''
+        #   org.gradle.console=verbose
+        #   org.gradle.daemon.idletimeout=3600000
+        # '';
 
+        ".zshrc".source = src/zsh/zshrc;
+
+        ".config/kitty".source = src/kitty;
+        ".config/fastfetch".source = src/fastfetch;
+        ".config/nvim".source = src/nvim;
     };
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
 
-  programs.home-manager.enable = true;
+    home.sessionVariables = {
+        EDITOR = "nvim";
+    };
+
+    programs.home-manager.enable = true;
 }
